fd5616534c222081f242510fa686d796425d08db
Uggeli <sakkisen.ville@gmail.com>
2024-11-10 15:23:31 +0200

    Remove IEntity interface and EntityPosition struct from EntitySystem; streamline entity management by eliminating unused components

--- GameEngine/EntitySystem/Entity.cs
+++
@@ -1,8 +0,0 @@
-namespace WebPeli.GameEngine.EntitySystem;
-
-public interface IEntity {
-    Guid Id { get; }
-} // Marker interface
-public readonly record struct EntityPosition(byte X, byte Y);
-
-
